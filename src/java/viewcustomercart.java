6b564f7bbc6029f56d86a8576891d47c66663777
KHSCI5MCA16076 <KHSCI5MCA16076@CS-241.asas.kh.amrita.edu>
2019-04-17 15:40:57 +0530

    change

--- src/java/viewcustomercart.java
+++ src/java/viewcustomercart.java
@@ -75,7 +75,6 @@ public class viewcustomercart extends HttpServlet {
                        +"</tr>");
                         
                     }
-                 //String id = rs.getString(1);
                  
                  out.println("/<table>");
                  out.println("<form action='shopproduct'><br><center>  \n" +
